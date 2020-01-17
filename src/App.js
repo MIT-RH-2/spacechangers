0dc2b95c09545873ce570c5a859ff6fe152f1921
Matthew Thompson <code.matthew.thompson@gmail.com>
2020-01-17 14:31:56 -0500

    A boloon renders and draggable out of the page

--- src/App.js
+++ src/App.js
@@ -4,21 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ flex: 1 }}>
+      <ml-model
+        extractable="true"
+        src="balloon.fbx"
+        style={{width: '300px', height: '300px'}}>
+      </ml-model>
     </div>
   );
 }
