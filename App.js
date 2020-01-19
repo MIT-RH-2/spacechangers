836ead812698abf79b56a08c23a1a55e6eb10d6d
Matthew Thompson <code.matthew.thompson@gmail.com>
2020-01-19 00:22:47 -0500

    taking what was salvagable for state from the magicscript magicleap work

--- App.js
+++ App.js
@@ -6,30 +6,68 @@
  * @flow
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {SafeAreaView, StyleSheet, StatusBar} from 'react-native';
 
 import {ARKit} from 'react-native-arkit';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView />
-      <ARKit
-        style={{flex: 1}}
-        debug
-        planeDetection={ARKit.ARPlaneDetection.Horizontal}
-        lightEstimationEnabled
-        detectionImages={[{resourceGroupName: 'DetectionImages'}]}
-        onARKitError={console.log} // if arkit could not be initialized (e.g. missing permissions), you will get notified here
-      />
-    </>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: props.message,
+      surfaces: [],
+      addingNewSurface: false,
+    };
+  }
 
-const styles = StyleSheet.create({
-  
-});
+  addSurface = points => {
+    let stateCopy = this.state.surfaces.concat([]);
+    stateCopy.push(points);
+    this.setState({
+      surfaces: stateCopy,
+    });
+  };
+
+  beginAddingSurface = () => {
+    this.setState({
+      addingNewSurface: true,
+      newSurfacePoints: {
+        topLeft: null,
+        topRight: null,
+        bottomLeft: null,
+        bottomRight: null,
+      },
+    });
+  };
+
+  addSurfacePoint = (hitPosition, cornerName) => {
+    this.setState({
+      newSurfacePoints: {
+        ...this.state.newSurfacePoints,
+        [cornerName]: hitPosition,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView />
+        <ARKit
+          style={{flex: 1}}
+          debug
+          planeDetection={ARKit.ARPlaneDetection.Horizontal}
+          lightEstimationEnabled
+          detectionImages={[{resourceGroupName: 'DetectionImages'}]}
+          onARKitError={console.log} // if arkit could not be initialized (e.g. missing permissions), you will get notified here
+        />
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
 
 export default App;
